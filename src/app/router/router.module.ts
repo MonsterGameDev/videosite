73c2e79531be391d9821f9e22be9e15a42ea7956
Per Høyer <per@brf.dk>
2018-11-26 20:56:20 +0100

    Azure #¤%&/()

--- src/app/router/router.module.ts
+++ src/app/router/router.module.ts
@@ -7,7 +7,7 @@ import { NotFoundComponent } from '../not-found/not-found.component';
 
 export const ROUTES = [
   { path: 'home', component: HomeComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: '**', component: NotFoundComponent }
 ];
 
